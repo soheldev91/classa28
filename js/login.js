9c8446d234cb14d02f6aa9bd0ee7dea149bf36f3
sohel <145013908+soheldev91@users.noreply.github.com>
2023-11-26 14:26:24 +0600

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,5 @@
+document.getElementById("login-button").addEventListener("click", function (){
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("email").value;
+    console.log(email, password);
+})
